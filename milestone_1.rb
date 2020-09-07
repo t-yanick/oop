898282e856fe213c4c7100202d5a5eaff0d2fb33
bmuhamood <bbosa2009@gmail.com>
2020-09-07 22:39:49 +0400

    added ruby method - Board

---
+++ milestone_1.rb
@@ -0,0 +1,12 @@
+def display_board(the_board)
+    puts " #{the_board[0]}| #{the_board[1]}| #{the_board[2]}"
+    puts "..........."
+    puts " #{the_board[3]}| #{the_board[4]}| #{the_board[5]}"
+    puts "..........."
+    puts " #{the_board[6]}| #{the_board[7]}| #{the_board[8]}"
+  end
+  
+
+
+
+  
