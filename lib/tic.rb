5613d22ddf802308c6d8a87db69c9016d7d32723
Yanick <tyankee15@gmail.com>
2020-09-14 12:43:53 -0300

    removed all puts and gets from the tic.rb file allowing just the game logic

--- lib/tic.rb
+++ lib/tic.rb
@@ -5,7 +5,7 @@ class Tic
   attr_accessor :board
 
   def display_board
-     "  #{@board[0]} | #{@board[1]} | #{@board[2]}"
+     puts "  #{@board[0]} | #{@board[1]} | #{@board[2]}"
      '  --+---+---'
      "  #{@board[3]} | #{@board[4]} | #{@board[5]}"
      '  --+---+---'
@@ -28,71 +28,74 @@ class Tic
         next
       end
 
-      if @board[tic_position[0]] == 'X'
-        "Hello #{player} have won the game!!!"
-      else
-        "Hello #{player1} have won the game!!!"
-      end
-      return true
-    end
-    false
+    #   if @board[tic_position[0]] == 'X'
+    #     "Hello #{player} have won the game!!!"
+    #   else
+    #     "Hello #{player1} have won the game!!!"
+    #   end
+    #   return true
+    # end
+    # false
   end
 end
+end
 
-choice = 'y'
-while choice == 'y'
-  'Welcome to Tic-Tac-Toe Game'
-  ''
+# choice = 'y'
+# while choice == 'y'
+#   'Welcome to Tic-Tac-Toe Game'
+#   ''
 
-  'Please Enter Your name:'
-  player = gets.strip
-  puts ''
+#   'Please Enter Your name:'
+#   player = gets.strip
+#   puts ''
 
-  puts "Please Enter opponent's name:"
-  player1 = gets.strip
+#   puts "Please Enter opponent's name:"
+#   player1 = gets.strip
+
   # check duplicate player entry
-  while player1 == player
-    puts "#{player} is already taken. Please enter a valid name:"
-    player1 = gets.strip
-  end
-  puts ''
 
-  game = Tic.new(player, player1)
-  turn = true
-  is_playing = true
+#   while player1 == player
+#     puts "#{player} is already taken. Please enter a valid name:"
+#     player1 = gets.strip
+#   end
+#   puts ''
 
-  while is_playing
-    game.display_board
+#   game = Tic.new(player, player1)
+#   turn = true
+#   is_playing = true
 
-    if turn
-      turn = false
-      loop do
-        puts "#{game.player}. Turn 1: Enter a valid position(1-9)"
-        tic_position = gets.strip.to_i
-        break if game.add_positions('X', tic_position)
-      end
-    else
-      turn = true
-      loop do
-        puts "#{game.player1} Turn 2: Enter a valid position(1-9)"
-        tic_position = gets.strip.to_i
-        break if game.add_positions('O', tic_position)
-      end
-    end
+#   while is_playing
+#     game.display_board
 
-    next unless game.check_winner
+#     if turn
+#       turn = false
+#       loop do
+#         puts "#{game.player}. Turn 1: Enter a valid position(1-9)"
+#         tic_position = gets.strip.to_i
+#         break if game.add_positions('X', tic_position)
+#       end
+#     else
+#       turn = true
+#       loop do
+#         puts "#{game.player1} Turn 2: Enter a valid position(1-9)"
+#         tic_position = gets.strip.to_i
+#         break if game.add_positions('O', tic_position)
+#       end
+#     end
 
-    puts ''
-    game.display_board
-    puts ''
-    is_playing = false
-  end
-  choice = nil
-  while choice != 'y' && choice != 'n'
-    puts 'Would you like to start a new game?(y/n)'
-    choice = gets.strip!
-  end
-end
+#     next unless game.check_winner
+
+#     puts ''
+#     game.display_board
+#     puts ''
+#     is_playing = false
+#   end
+#   choice = nil
+#   while choice != 'y' && choice != 'n'
+#     puts 'Would you like to start a new game?(y/n)'
+#     choice = gets.strip!
+#   end
+# end
 
 # rubocop : enable Layout/LineLength
 # rubocop : enable Metrics/BlockNesting
