68e9bd5c667286389fbad4c063dcd2b9d81a1860
Yanick <tyankee15@gmail.com>
2020-09-14 13:44:24 -0300

    refactoring code to end when a draw is detected

--- lib/tic.rb
+++ lib/tic.rb
@@ -2,15 +2,19 @@
 # rubocop : disable Metrics/BlockNesting
 
 class Tic
-  attr_accessor :board
+  attr_reader :board
 
   def display_board
-     puts "  #{@board[0]} | #{@board[1]} | #{@board[2]}"
+     "  #{@board[0]} | #{@board[1]} | #{@board[2]}"
      '  --+---+---'
      "  #{@board[3]} | #{@board[4]} | #{@board[5]}"
      '  --+---+---'
      "  #{@board[6]} | #{@board[7]} | #{@board[8]}"
      ''
+  end
+
+  def full
+    @board.all?(String)
   end
 
   def add_positions(piece, tic_position)
