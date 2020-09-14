4745d2315e1ccbb102a0a6bd504a4137ae52a5b5
bmuhamood <bbosa2009@gmail.com>
2020-09-14 23:07:33 +0400

    removed section asking user replay the game

--- lib/tic.rb
+++ lib/tic.rb
@@ -2,104 +2,72 @@
 # rubocop : disable Metrics/BlockNesting
 
 class Tic
-  attr_reader :board
-
-  def display_board
-     "  #{@board[0]} | #{@board[1]} | #{@board[2]}"
-     '  --+---+---'
-     "  #{@board[3]} | #{@board[4]} | #{@board[5]}"
-     '  --+---+---'
-     "  #{@board[6]} | #{@board[7]} | #{@board[8]}"
-     ''
+  attr_reader :winner
+  def initialize
+    @usrboard = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+    @move_number = 0
+    @winner = false
   end
 
-  def display_board
-    @board.all?(String)
-  end
-  
-  def add_positions(piece, tic_position)
-    if tic_position < 1 || tic_position > 9 || @board[tic_position - 1] == 'X' || @board[tic_position - 1] == 'O'
-      false
-    else
-      @board[tic_position - 1] = piece
-      true
-    end
+  def current_board
+    "\n #{@usrboard[0][0]} | #{@usrboard[0][1]} | #{@usrboard[0][2]} \n"\
+    "----------- \n"\
+    " #{@usrboard[1][0]} | #{@usrboard[1][1]} | #{@usrboard[1][2]} \n"\
+    "----------- \n"\
+    " #{@usrboard[2][0]} | #{@usrboard[2][1]} | #{@usrboard[2][2]} \n"
   end
 
-  def check_winner
-    @winner.each do |tic_position|
-      unless @board[tic_position[0]] == @board[tic_position[1]] && @board[tic_position[1]] == @board[tic_position[2]] && (@board[tic_position[0]] == 'X' || @board[tic_position[0]] == 'O')
-        next
+  def move(x_pos, y_pos, player2)
+    @usrboard[x_pos][y_pos] = player2
+    @move_number += 1
+    check_winner(x_pos, y_pos, player2) if @move_number > 4
+  end
+
+  def check_winner(x_pos, y_pos, player2)
+    @winner = check_row(x_pos, player2)
+    return @winner if @winner
+
+    @winner = check_col(y_pos, player2)
+    return @winner if @winner
+
+    @winner = check_diagonals(x_pos, y_pos, player2) if x_pos == y_pos || (x_pos - y_pos).abs == 2
+  end
+
+  def reset_board
+    @usrboard = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+    @move_number = 0
+    @winner = false
+  end
+
+  def empty_position?(x_pos, y_pos)
+    @usrboard[x_pos][y_pos] == ' '
+  end
+
+  private
+
+  def check_row(y_pos, player2)
+    @usrboard[y_pos].all? { |i| i == player2 }
+  end
+
+  def check_col(x_pos, player2)
+    @usrboard.all? { |i| i[x_pos] == player2 }
+  end
+
+  def check_diagonals(x_pos, y_pos, player2)
+    if (x_pos - y_pos).abs == 2
+      j = 2
+      3.times do |i|
+        return false unless @usrboard[i][j] == player2
+
+        j -= 1
       end
+      return true
+    end
 
-    #   if @board[tic_position[0]] == 'X'
-    #     "Hello #{player} have won the game!!!"
-    #   else
-    #     "Hello #{player1} have won the game!!!"
-    #   end
-    #   return true
-    # end
-    # false
+    3.times { |i| return false unless @usrboard[i][i] == player2 }
+    true
   end
 end
-end
-
-# choice = 'y'
-# while choice == 'y'
-#   'Welcome to Tic-Tac-Toe Game'
-#   ''
-
-#   'Please Enter Your name:'
-#   player = gets.strip
-#   puts ''
-
-#   puts "Please Enter opponent's name:"
-#   player1 = gets.strip
-
-  # check duplicate player entry
-
-#   while player1 == player
-#     puts "#{player} is already taken. Please enter a valid name:"
-#     player1 = gets.strip
-#   end
-#   puts ''
-
-#   game = Tic.new(player, player1)
-#   turn = true
-#   is_playing = true
-
-#   while is_playing
-#     game.display_board
-
-#     if turn
-#       turn = false
-#       loop do
-#         puts "#{game.player}. Turn 1: Enter a valid position(1-9)"
-#         tic_position = gets.strip.to_i
-#         break if game.add_positions('X', tic_position)
-#       end
-#     else
-#       turn = true
-#       loop do
-#         puts "#{game.player1} Turn 2: Enter a valid position(1-9)"
-#         tic_position = gets.strip.to_i
-#         break if game.add_positions('O', tic_position)
-#       end
-#     end
-
-#     next unless game.check_winner
-
-#     puts ''
-#     game.display_board
-#     puts ''
-#     is_playing = false
-#   end
-#   choice = nil
-#   while choice != 'y' && choice != 'n'
-#     puts 'Would you like to start a new game?(y/n)'
-#     choice = gets.strip!
-#   end
-# end
 
 # rubocop : enable Layout/LineLength
 # rubocop : enable Metrics/BlockNesting
